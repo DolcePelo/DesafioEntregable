98bcba294d976e2104d06672c1aa717813f69873
Alejandro Pelozatto <alejandrojpelozatto@gmail.com>
2023-11-28 13:26:03 -0300

    desafio entregable 2.

--- ProductManager.js
+++ ProductManager.js
@@ -125,31 +125,3 @@ async function test () {
 }
 test()
 
-// const productoUno = new ProductManager('./products.json');
-// const productoDos = new ProductManager('./products.json');
-// const productoTres = new ProductManager('./products.json');
-
-// productoUno.addProducts(
-//     'Televisor',
-//     'HD pantalla curva',
-//     1500,
-//     'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.muycomputer.com%2F2016%2F01%2F20%2Ftelevisores-pantalla-curva-ventajas%2F&psig=AOvVaw2Ga2vJSbmwDAyzjynjknQu&ust=1700262772492000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCOCCj8XSyYIDFQAAAAAdAAAAABAH',
-//     20,
-// );
-
-// productoDos.addProducts(
-//     'Celular',
-//     'Samsung A53',
-//     600,
-//     'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.elespanol.com%2Felandroidelibre%2Fmoviles-android%2F20220330%2Fmotivos-comprar-samsung-galaxy-a53-ventajas-inconvenientes%2F661184245_0.html&psig=AOvVaw2lT_I2_FHE2J1Hn9AsXzRM&ust=1700264648075000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCPiv7sLZyYIDFQAAAAAdAAAAABAF',
-//     23,
-// );
-
-// productoTres.addProducts(
-//     'Table',
-//     'Samsung E20',
-//     500,
-//     'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.elespanol.com%2Felandroidelibre%2Fmoviles-android%2F20220330%2Fmotivos-comprar-samsung-galaxy-a53-ventajas-inconvenientes%2F661184245_0.html&psig=AOvVaw2lT_I2_FHE2J1Hn9AsXzRM&ust=1700264648075000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCPiv7sLZyYIDFQAAAAAdAAAAABAF',
-//     23,
-// );
-
